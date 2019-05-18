2e640ab8512b9feaf7682d538bd438f9e0c91da3
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-18 11:12:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,10 @@ def my_collect(array)
   num = 0
   alen = array.size
   arr = []
-
   while num < alen
-    arr << yield(array[num])
+    arr = yield(array[num])
     num += 1
   end
   arr
-
+    
 end
